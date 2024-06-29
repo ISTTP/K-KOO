a6fe84cd269ad53c56ef74773f369564fc18f5cc
ieun32 <ueunviss@gmail.com>
2024-06-29 11:08:16 +0900

    Feat: Login, Signup 페이지 생성

--- apps/client/src/pages/Signup.tsx
+++ apps/client/src/pages/Signup.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../components/Wrapper'
 
 function SignUp() {
   return (
-    <>
+    <Wrapper>
       <h1>Sign Up</h1>
       <Link to="/">Login</Link>
-    </>
+    </Wrapper>
   )
 }
 
