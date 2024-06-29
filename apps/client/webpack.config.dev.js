d4af55bb1b9dc3dd6c327f63010512cc7762c616
ieun32 <ueunviss@gmail.com>
2024-06-29 11:05:46 +0900

    Chore: dotenv 설정

--- apps/client/webpack.config.dev.js
+++ apps/client/webpack.config.dev.js
@@ -1,69 +1,76 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        main: path.resolve(__dirname, "src", "index.tsx"),
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true
-    },
-    devServer: { // 개발 서버 설정
-        static: path.resolve(__dirname, "dist"),
-        port: 80,
-        open: true, // 기본 브라우저에서 실행
-        historyApiFallback: true, // connect-history-api-fallback error 방지?
-    },
-    resolve: {
-        extensions: [".js", ".ts", ".tsx", ".jsx"],
-    },
-    target: "web",
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: "ts-loader",
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            "@babel/preset-react",
-                            "@babel/preset-typescript",
-                        ],
-                        plugins: [
-                            [
-                                "@babel/plugin-transform-runtime",
-                                {
-                                    "corejs": 3,
-                                    "regenerator": true,
-                                }
-                            ]
-                        ]
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|ttf)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
-        }),
+  mode: "development",
+  entry: {
+    main: path.resolve(__dirname, "src", "index.tsx"),
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  devServer: { // 개발 서버 설정
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    open: true, // 기본 브라우저에서 실행
+    historyApiFallback: true, // connect-history-api-fallback error 방지?
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx", ".jsx"],
+  },
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader"
+        },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+            plugins: [
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  "corejs": 3,
+                  "regenerator": true,
+                }
+              ]
+            ]
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|ttf)$/i,
+        type: "asset/resource",
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    // https://webpack.kr/plugins/environment-plugin/
+    new Dotenv({
+      path: './.env'
+    })
+  ],
 }
