e6cd7517062a4bd3307aa47d829859cb2ab78ed0
ieun32 <ueunviss@gmail.com>
2024-07-04 20:58:50 +0900

    Feat: 서버 라우트 설정

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,35 +1,15 @@
 import { createServer } from "./server";
-import { PrismaClient } from "@repo/db";
-import { User } from "@repo/types";
-import { add } from "@repo/utils";
 import { log } from "@repo/logger";
+
+import auth from "./routes/auth";
 
 import "dotenv/config";
 
-const prisma = new PrismaClient();
-
-const port = process.env.SERVER_PORT || 3001;
+const port = process.env.SERVER_PORT;
 const server = createServer();
-
-const user: User = {
-  id: 1,
-  name: "John Doe",
-  email: "John@naver.com",
-};
-
-async function getUsers() {
-  const users = await prisma.user.findMany();
-  log(add(1, 2));
-  log(user);
-  log(users);
-  return users;
-}
 
 server.listen(port, () => {
   log(`api running on ${port}`);
-  getUsers();
 });
 
-server.get("/api/test", function (req, res) {
-  res.send("하이");
-});
+server.use("/api", auth);
