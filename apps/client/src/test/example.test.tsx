08afec1f5fd1ddba42de9cbb703e098aba6cd883
ieun32 <ueunviss@gmail.com>
2024-07-06 18:03:11 +0900

    Chore: logger 삭제

--- apps/client/src/test/example.test.tsx
+++ apps/client/src/test/example.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { log } from '@repo/logger';
 import Button from '../components/Button';
 
 test('카카오 버튼 렌더링', () => {
@@ -9,7 +8,7 @@ test('카카오 버튼 렌더링', () => {
     <Button
       type="kakao"
       label="Kakao로 시작하기"
-      onClick={() => log('카카오')}
+      onClick={() => console.log('카카오')}
     />,
   );
   const buttonElement = screen.getByText(/Kakao로 시작하기/i);
