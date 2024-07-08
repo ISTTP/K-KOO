dbed37cb60e4628dc524308e6a90db6588424669
ieun32 <ueunviss@gmail.com>
2024-07-08 12:26:27 +0900

    Feat: 토큰 검증 유틸 함수 작성

--- packages/utils/src/jwt.ts
+++ packages/utils/src/jwt.ts
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@isttp/db/all';
 import {
   JsonWebTokenError,
   TokenExpiredError,
@@ -6,9 +7,11 @@ import {
 } from 'jsonwebtoken';
 import 'dotenv/config';
 
-export function generateAccessToken(id: string) {
-  const accessToken = jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
-    expiresIn: 60 * 60,
+const prisma = new PrismaClient();
+
+export function generateAccessToken(userId: number) {
+  const accessToken = jwt.sign({ userId }, `${process.env.JWT_SECRET}`, {
+    expiresIn: 1,
     algorithm: 'HS256',
   });
 
@@ -29,20 +32,71 @@ export function verifyToken(token: string) {
     return jwt.verify(token, process.env.JWT_SECRET as string);
   } catch (error) {
     if (error instanceof TokenExpiredError) {
-      return new Error('EXPIRED');
+      throw new Error('EXPIRED');
     } else if (error instanceof JsonWebTokenError) {
-      return new Error('INVALID');
+      throw new Error('INVALID');
     } else if (error instanceof NotBeforeError) {
-      return new Error('NOTBEFORE');
+      throw new Error('NOTBEFORE');
     } else {
-      return new Error('UNKNOWN');
+      throw new Error('UNKNOWN');
     }
   }
 }
 
-export function reissueToken(id: string) {
-  const accessToken = generateAccessToken(id);
+export function reissueToken(userId: number) {
+  const accessToken = generateAccessToken(userId);
   const refreshToken = generateRefreshToken();
 
   return { accessToken, refreshToken };
+}
+
+// 액세스 토큰이 유효하면 true
+// 액세스 토큰이 만료되면 리프레시 토큰 검증
+// 리프레시 토큰이 유효하면 새로운 액세스 토큰, 리프레시 토큰 발급, true, 리프레시 토큰 업뎃, set쿠키
+// 리프레시 토큰도 만료되었으면 false -> 로그인페이지로 ㄱㄱ
+
+export async function checkValidation({
+  userId,
+  accessToken,
+  refreshToken,
+}: {
+  userId: number;
+  accessToken: string;
+  refreshToken: string;
+}) {
+  try {
+    verifyToken(accessToken);
+    return { message: 'ACCESS_VALID' };
+  } catch (error) {
+    if (error instanceof Error) {
+      switch (error.message) {
+        case 'EXPIRED': {
+          const decoded = verifyToken(refreshToken);
+          if (decoded instanceof Error) {
+            return { message: 'EXPIRED' };
+          }
+          const tokens = reissueToken(userId);
+          const newAccessToken = tokens.accessToken;
+          const newRefreshToken = tokens.refreshToken;
+
+          await prisma.user.update({
+            where: {
+              userId,
+            },
+            data: {
+              refreshToken: newRefreshToken,
+            },
+          });
+          return {
+            message: 'REFRESH_VALID',
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken,
+          };
+        }
+
+        default:
+          return { message: 'EXPIRED' };
+      }
+    }
+  }
 }
