efc92ff0b51610401704a143bc7d1098560acf71
kyr4601 <jane4545@naver.com>
2024-07-10 19:15:38 +0900

    Feat: 케이크 페이지 버전 구분을 위한 api구현

--- apps/client/src/pages/Cake.tsx
+++ apps/client/src/pages/Cake.tsx
@@ -3,6 +3,7 @@ import Wrapper from '#components/Wrapper.tsx';
 import Toggle from '#components/Toggle.tsx';
 import GridInfo from '#components/GridInfo.tsx';
 import CakeInfo from '#components/CakeInfo.tsx';
+import axiosInstance from '#apis/axios.ts';
 
 const Cake = () => {
   const [toggle, setToggle] = useState(false);
@@ -10,11 +11,16 @@ const Cake = () => {
     setToggle((prev) => !prev);
   };
 
+  async function test() {
+    const res = await axiosInstance.get('cake/version?cakeUserId=sean5rum4l');
+    console.log(res.data);
+  }
+
   return (
     <Wrapper>
       <h3>김예린 님의 케이크</h3>
       <p>장식초를 눌러 편지를 확인해보세요</p>
-      <button>my</button>
+      <button onClick={test}>my</button>
       <Toggle toggle={toggle} onClick={clickedToggle} />
       {/* toggle에 따라 다른 컴포넌트 렌더링 */}
       {toggle ? <GridInfo /> : <CakeInfo />}
