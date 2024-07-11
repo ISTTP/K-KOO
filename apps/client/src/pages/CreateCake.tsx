5dab6a89ff5d2c5a748713857d31634e6a935da8
ieun32 <ueunviss@gmail.com>
2024-07-11 13:02:42 +0900

    Feat: 케이크 시트, 크림 정보 저장 후 케이크 페이지로 리다이렉트

--- apps/client/src/pages/CreateCake.tsx
+++ apps/client/src/pages/CreateCake.tsx
@@ -1,18 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '#apis/axios.ts';
+
+import Button from '#components/Button.tsx';
 import Wrapper from '#components/Wrapper.tsx';
-import ColorSelector from '#components/ColorSelector.tsx';
 import RenderCake from '#components/RenderCake.tsx';
+import ColorSelector from '#components/ColorSelector.tsx';
 
 import { CakeColorType } from '@isttp/types/all';
-import Button from '#components/Button.tsx';
-
-function handleCreateCake() {
-  console.log('케이크 만들기!');
-}
 
 const CreateCake = () => {
+  const navigate = useNavigate();
   const [sheetColor, setSheetColor] = useState<CakeColorType>('white');
   const [creamColor, setCreamColor] = useState<CakeColorType>('chocolate');
+
+  async function handleCreateCake() {
+    try {
+      console.log(sheetColor, creamColor);
+      const res = await axiosInstance.post('/cake/color', {
+        sheetColor: sheetColor,
+        creamColor: creamColor,
+      });
+
+      if (res.status === 200) navigate('/cake');
+      else throw new Error('케이크 만들기에 실패했습니다.');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    try {
+      axiosInstance.get('/cake/color').then((res) => {
+        if (!res.data.sheetColor || !res.data.creamColor) {
+          setSheetColor('white');
+          setCreamColor('chocolate');
+        }
+
+        setSheetColor(res.data.sheetColor);
+        setCreamColor(res.data.creamColor);
+      });
+    } catch (error) {
+      console.log(error);
+      // 로그인이 필요합니다 모달 어떤 에러인지에 따라 모달 달리 하기
+    }
+  }, []);
 
   return (
     <Wrapper>
