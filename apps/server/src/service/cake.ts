56cc8669a4585c4b380040d216310751c3c2d93b
ieun32 <ueunviss@gmail.com>
2024-07-11 13:13:09 +0900

    Feat: 케이크 크림, 시트 색상 정보 조회/수정 함수 작성

---
+++ apps/server/src/service/cake.ts
@@ -0,0 +1,33 @@
+import { getUser, setUser } from '../models/user';
+
+export async function getCakeColor(userId: string) {
+  try {
+    const userInfo = await getUser(userId);
+    if (!userInfo) throw new Error('사용자 정보를 찾을 수 없습니다.');
+    const { sheetColor, creamColor } = userInfo;
+    return { sheetColor, creamColor };
+  } catch (error) {
+    throw new Error('색상 정보를 불러오는데 실패했습니다.');
+  }
+}
+
+export async function setCakeColor({
+  userId,
+  sheetColor,
+  creamColor,
+}: {
+  userId: string;
+  sheetColor: string;
+  creamColor: string;
+}) {
+  try {
+    await setUser(userId, {
+      sheetColor,
+      creamColor,
+    });
+
+    return { message: '색상 정보를 저장했습니다.' };
+  } catch (error) {
+    throw new Error('색상 정보를 저장하는데 실패했습니다.');
+  }
+}
