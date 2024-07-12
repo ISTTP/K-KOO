23dff187cf1a010207152db036aa232b2cad38e0
ieun32 <ueunviss@gmail.com>
2024-07-12 20:38:50 +0900

    Feat: 케이크 색상 정보 받아오기, 편지 작성 및 내 케이크 이동 버튼 생성

--- apps/client/src/components/cake/SharedCake.tsx
+++ apps/client/src/components/cake/SharedCake.tsx
@@ -1,19 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '#apis/axios.ts';
 import CakeInfo from '#components/cake/CakeInfo.tsx';
 import Wrapper from '#components/Wrapper.tsx';
-import { CakeUserTypeResponse } from '@isttp/types/all';
+import { CakeUserTypeResponse, CakeColorType } from '@isttp/types/all';
+import Button from '#components/Button.tsx';
+import { AxiosError } from 'axios';
 
 interface MyCakeProps {
+  ownerId: string;
   data: CakeUserTypeResponse;
 }
 
-const SharedCake: React.FC<MyCakeProps> = ({ data }) => {
+type CakeColorState = {
+  sheetColor: CakeColorType;
+  creamColor: CakeColorType;
+};
+
+const SharedCake: React.FC<MyCakeProps> = ({ ownerId, data }) => {
+  const [cakeColor, setCakeColor] = useState<CakeColorState>({
+    sheetColor: 'chocolate',
+    creamColor: 'white',
+  });
+
+  async function getColors(userId: string) {
+    try {
+      const res = await axiosInstance.get(`/cake/color/${userId}`);
+
+      if (!res.data.sheetColor || !res.data.creamColor) return;
+
+      setCakeColor({
+        sheetColor: res.data.sheetColor,
+        creamColor: res.data.creamColor,
+      });
+    } catch (error) {
+      console.log(error);
+      // 에러 코드에 따라 모달
+    }
+  }
+
+  const navigate = useNavigate();
+
+  function handleCreateLetter() {
+    navigate(`/letter/create/${ownerId}`);
+  }
+
+  async function handleNavigateToMyCake() {
+    // 토큰 유효성 확인 후 반환받은 유저 아이디 케이크 페이지로 이동
+    axiosInstance
+      .get('/user/me')
+      .then((res) => {
+        if (res.status === 200) {
+          navigate(`/cake/${res.data.userId}`);
+        }
+      })
+      .catch((error) => {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 401) {
+            alert('로그인이 필요합니다.');
+          }
+        }
+      });
+  }
+
+  useEffect(() => {
+    getColors(ownerId);
+  }, [ownerId]);
+
   return (
     <Wrapper>
       <h3>{data.nickname} 님의 케이크</h3>
       <p>친구의 케이크를 꾸며보세요!</p>
-
-      <CakeInfo year={data.year} />
+      <CakeInfo
+        year={data.year}
+        sheetColor={cakeColor.sheetColor}
+        creamColor={cakeColor.creamColor}
+      />
+      <Button
+        type="default"
+        label="케이크 꾸며주기"
+        onClick={handleCreateLetter}
+      />
+      <Button
+        type="default"
+        label="내 케이크 보러가기"
+        onClick={handleNavigateToMyCake}
+      />
     </Wrapper>
   );
 };
