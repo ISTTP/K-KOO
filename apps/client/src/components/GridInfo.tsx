b4da0a49007cecbff7727c703ecbf765317789f3
kyr4601 <jane4545@naver.com>
2024-07-12 21:11:39 +0900

    Feat: react-window 사용해 grid 구현

--- apps/client/src/components/GridInfo.tsx
+++ apps/client/src/components/GridInfo.tsx
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState, CSSProperties } from 'react';
+import axiosInstance from '#apis/axios.ts';
+import { CakeTypeResponse } from '@isttp/types/all';
+import { useParams } from 'react-router-dom';
+import * as G from '#components/GridStyle.tsx';
+import { FixedSizeGrid as Grid } from 'react-window';
 
-const GridInfo = () => {
-  return <div>그리드</div>;
+interface yearProp {
+  year: string;
+}
+
+const GridInfo: React.FC<yearProp> = ({ year }) => {
+  const Cell = ({
+    columnIndex,
+    rowIndex,
+    style,
+  }: {
+    columnIndex: number;
+    rowIndex: number;
+    style: CSSProperties;
+  }) => {
+    const index = rowIndex * 3 + columnIndex;
+    if (index >= cakeData.length) return null;
+
+    const item = cakeData[index];
+    return (
+      <div
+        className={
+          columnIndex % 2
+            ? rowIndex % 2 === 0
+              ? 'GridItemOdd'
+              : 'GridItemEven'
+            : rowIndex % 2
+              ? 'GridItemOdd'
+              : 'GridItemEven'
+        }
+        style={{
+          ...style,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <G.CandleImage src={item.candleImageUrl} alt="장식초" />
+        <G.Keyword># {item.keyword}</G.Keyword>
+        <G.Nickname>{item.nickname}</G.Nickname>
+      </div>
+    );
+  };
+
+  const [cakeData, setCakeData] = useState<CakeTypeResponse[]>([]);
+  const { cakeUserId } = useParams();
+
+  async function getLetters() {
+    const res = await axiosInstance.get(
+      `/cake/${cakeUserId}/${year}?keyword=true&page=all`,
+    );
+    setCakeData(res.data.data);
+  }
+  useEffect(() => {
+    getLetters();
+  }, []);
+
+  return (
+    <>
+      <Grid
+        columnCount={3} //3xN의 3
+        columnWidth={100} //3개가 모두 포함되는 가로 사이즈
+        height={500} //렌더링되는 높이
+        rowCount={Math.ceil(cakeData.length / 3)} //3xN의 N
+        rowHeight={150} //3개가 모두 포함되는 세로 사이즈
+        width={300} //렌더링되는 넓이
+      >
+        {Cell}
+      </Grid>
+    </>
+  );
 };
 
 export default GridInfo;
---
+++ apps/client/src/components/GridStyle.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const GridItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 8px;
+  border-radius: 8px;
+  background-color: var(--light-orange-color);
+`;
+
+export const CandleImage = styled.img`
+  width: 70px;
+  height: 70px;
+  object-fit: cover;
+  margin-bottom: 4px;
+`;
+
+export const Nickname = styled.p`
+  font-size: 14px;
+  padding: 0;
+  margin: 0;
+  color: var(--black-color);
+`;
+
+export const Keyword = styled.p`
+  font-size: 14px;
+  padding: 0;
+  margin: 0;
+  color: var(--black-color);
+`;
