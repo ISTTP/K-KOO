4cc062f8140604d2dc320c3a7bad79de581052bb
ieun32 <ueunviss@gmail.com>
2024-07-15 17:51:30 +0900

    Feat: 장식초 고르기 페이지 생성

--- apps/client/src/pages/ChooseCandle.tsx
+++ apps/client/src/pages/ChooseCandle.tsx
@@ -3,13 +3,29 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axiosInstance from '#apis/axios.ts';
 import styled from 'styled-components';
 import Wrapper from '#components/Wrapper.tsx';
+import { CandleType, CandleResponseType } from '@isttp/schemas/all';
+
+const CandleContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+  justify-items: center;
+  align-items: center;
+  margin-top: 2rem;
+`;
 
 const CandleButton = styled.button`
   width: 5rem;
   height: 5rem;
-  border-radius: 2rem;
+  border-radius: 1rem;
+  border-width: 0;
   display: relative;
   overflow: hidden;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 5px 0 var(--orange-color);
+  }
 `;
 
 const CandleImage = styled.img`
@@ -19,23 +35,19 @@ const CandleImage = styled.img`
 
 async function getCandles() {
   try {
-    const res = await axiosInstance.get('/candles');
+    const res = await axiosInstance.get<CandleResponseType>('/candle');
+    CandleResponseType.parse(res.data);
     return res.data;
   } catch (error) {
     return null;
   }
 }
 
-type CandleData = {
-  id: number;
-  imageUrl: string;
-  point: number;
-}[];
-
 const ChooseCandle = () => {
   const { ownerId } = useParams();
   const navigate = useNavigate();
-  const [candles, setCandles] = useState<CandleData | null>(null);
+  //const [senderInfo, setSenderInfo] = useState<UserType | null>(null);
+  const [candles, setCandles] = useState<CandleType[] | null>(null);
 
   useEffect(() => {
     getCandles().then((data) => setCandles(data));
@@ -45,17 +57,30 @@ const ChooseCandle = () => {
 
   return (
     <Wrapper>
-      <h1>편지지 고르기</h1>
-      {candles?.map((candle) => (
-        <CandleButton
-          key={candle.id}
-          onClick={() =>
-            navigate(`/letter/create/${ownerId}?candle=${candle.id}`)
-          }
-        >
-          <CandleImage src={candle.imageUrl} alt={candle.id.toString()} />
-        </CandleButton>
-      ))}
+      <h1>장식초 고르기</h1>
+      <CandleContainer>
+        {candles?.map((candle, i) => (
+          <CandleButton
+            key={i}
+            onClick={() => {
+              // 무료 장식초라면 바로 편지 페이지로 이동
+              if (candle.point === 0) {
+                navigate(
+                  `/letter/create/${ownerId}?candleId=${candle.candleId}`,
+                );
+                // 유료 장식초라면 모달 띄우기
+              } else {
+                alert('포인트 결제 기능 준비중');
+              }
+            }}
+          >
+            <CandleImage
+              src={candle.imageUrl}
+              alt={candle.candleId.toString()}
+            />
+          </CandleButton>
+        ))}
+      </CandleContainer>
     </Wrapper>
   );
 };
