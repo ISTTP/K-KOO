423fcff5760ab04a1036079716c8006d73a12636
ieun32 <ueunviss@gmail.com>
2024-07-16 12:13:13 +0900

    Feat: Home, Messenger 아이콘 추가

--- apps/client/src/assets/icons/index.ts
+++ apps/client/src/assets/icons/index.ts
@@ -3,5 +3,15 @@ import KakaoLogoIcon from './KakaoLogoIcon';
 import GoogleIcon from './GoogleIcon';
 import KakaoIcon from './KakaoIcon';
 import LinkIcon from './LinkIcon';
+import HomeIcon from './HomeIcon';
+import MessengerIcon from './MessengerIcon';
 
-export { FacebookLogoIcon, KakaoLogoIcon, GoogleIcon, KakaoIcon, LinkIcon };
+export {
+  FacebookLogoIcon,
+  KakaoLogoIcon,
+  GoogleIcon,
+  KakaoIcon,
+  LinkIcon,
+  HomeIcon,
+  MessengerIcon,
+};
