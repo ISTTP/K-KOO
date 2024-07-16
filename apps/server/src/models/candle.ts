6e116a4c3c578755f7a0299ca84d42ccb6aa2f24
ieun32 <ueunviss@gmail.com>
2024-07-16 10:20:25 +0900

    Feat: 장식초 1개 조회 함수 작성

--- apps/server/src/models/candle.ts
+++ apps/server/src/models/candle.ts
@@ -4,3 +4,11 @@ const prisma = new PrismaClient();
 export async function getCandles() {
   return await prisma.candle.findMany();
 }
+
+export async function getCandle(candleId: number) {
+  return await prisma.candle.findUnique({
+    where: {
+      candleId,
+    },
+  });
+}
