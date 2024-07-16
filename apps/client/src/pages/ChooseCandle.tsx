acf9d8660d1d874b0d77becb140f2eba9db59942
ieun32 <ueunviss@gmail.com>
2024-07-16 10:25:29 +0900

    Feat; 장식초 선택 페이지 로직 작성

--- apps/client/src/pages/ChooseCandle.tsx
+++ apps/client/src/pages/ChooseCandle.tsx
@@ -3,7 +3,11 @@ import { useNavigate, useParams } from 'react-router-dom';
 import axiosInstance from '#apis/axios.ts';
 import styled from 'styled-components';
 import Wrapper from '#components/Wrapper.tsx';
-import { CandleType, CandleResponseType } from '@isttp/schemas/all';
+import LoginModal from '#components/modal/LoginModal.tsx';
+import { AxiosError } from 'axios';
+import { CandleType, CandleResponseType, UserType } from '@isttp/schemas/all';
+import Modal from '#components/modal/Modal.tsx';
+import Button from '#components/Button.tsx';
 
 const CandleContainer = styled.div`
   display: grid;
@@ -15,22 +19,33 @@ const CandleContainer = styled.div`
 `;
 
 const CandleButton = styled.button`
+  position: relative;
   width: 5rem;
   height: 5rem;
-  border-radius: 1rem;
+  border-radius: 0.5rem;
   border-width: 0;
   display: relative;
   overflow: hidden;
   cursor: pointer;
 
   &:hover {
-    box-shadow: 0 0 5px 0 var(--orange-color);
+    border: 2px solid var(--orange-color);
   }
 `;
 
 const CandleImage = styled.img`
   width: 100%;
   height: 100%;
+`;
+
+const CandleBadge = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: var(--orange-color);
+  color: var(--white-color);
+  padding: 0.2rem 0.5rem;
+  border-radius: 0 0 0 1rem;
 `;
 
 async function getCandles() {
@@ -46,14 +61,71 @@ async function getCandles() {
 const ChooseCandle = () => {
   const { ownerId } = useParams();
   const navigate = useNavigate();
-  //const [senderInfo, setSenderInfo] = useState<UserType | null>(null);
+  const [openBuy, setOpenBuy] = useState(false);
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [openLogin, setOpenLogin] = useState(false);
+  const [candle, setCandle] = useState<CandleType | null>(null);
   const [candles, setCandles] = useState<CandleType[] | null>(null);
+  const [userPoint, setUserPoint] = useState<number | null>(null);
 
   useEffect(() => {
     getCandles().then((data) => setCandles(data));
   }, []);
 
-  // 포인트로 결제 기능 구현 필요
+  function handleOpenBuy() {
+    setOpenBuy(!openBuy);
+  }
+
+  function handleOpenLogin() {
+    setOpenLogin(!openLogin);
+  }
+
+  async function openModal(candleId: number) {
+    // 선택한 장식초 정보 가져오기
+    axiosInstance.get(`/candle/${candleId}`).then((res) => {
+      if (res.status === 200) {
+        CandleType.parse(res.data);
+        setCandle(res.data);
+      } else {
+        throw new Error('장식초 정보를 가져오는데 실패했습니다.');
+      }
+    });
+    // 토큰 유효성 확인 후 결제 모달
+    axiosInstance
+      .get('/user/me')
+      .then((res) => {
+        if (res.status === 200) {
+          setUserPoint(res.data.point);
+          setOpenBuy(true);
+        }
+      })
+      // 권한 없을 경우 로그인 유도 모달
+      .catch((error) => {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 401) {
+            setOpenLogin(true);
+          }
+        }
+      });
+  }
+
+  async function handleBuyCandle(point: number) {
+    try {
+      const res = await axiosInstance.post<UserType>('/candle', { point });
+      UserType.parse(res.data);
+
+      if (res.status === 200) {
+        setUserPoint(res.data.point);
+        setOpenSuccess(true);
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 500) {
+          alert('구매 실패');
+        }
+      }
+    }
+  }
 
   return (
     <Wrapper>
@@ -63,17 +135,19 @@ const ChooseCandle = () => {
           <CandleButton
             key={i}
             onClick={() => {
-              // 무료 장식초라면 바로 편지 페이지로 이동
               if (candle.point === 0) {
+                // 무료 장식초: 편지 페이지로 이동
                 navigate(
                   `/letter/create/${ownerId}?candleId=${candle.candleId}`,
                 );
-                // 유료 장식초라면 모달 띄우기
               } else {
-                alert('포인트 결제 기능 준비중');
+                // 유료 장식초: 결제 혹은 로그인 유도 모달 띄우기
+                openModal(candle.candleId);
               }
             }}
           >
+            {candle.point === 0 && <CandleBadge>free</CandleBadge>}
+            {candle.point !== 0 && <CandleBadge>{candle.point}p</CandleBadge>}
             <CandleImage
               src={candle.imageUrl}
               alt={candle.candleId.toString()}
@@ -81,6 +155,64 @@ const ChooseCandle = () => {
           </CandleButton>
         ))}
       </CandleContainer>
+      <Modal open={openBuy}>
+        <img
+          style={{ width: '5rem', height: '5rem' }}
+          src={candle?.imageUrl}
+          alt={candle?.candleId.toString()}
+        />
+        <h3>해당 장식초를 구매하시겠습니까?</h3>
+        <span>남은 포인트: {userPoint}P</span>
+        <span>결제 포인트: {candle?.point}P</span>
+        {userPoint && candle?.point && userPoint < candle?.point && (
+          <span style={{ color: 'red' }}>포인트가 부족합니다.</span>
+        )}
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '100%',
+            gap: '1rem',
+          }}
+        >
+          <Button
+            type="gray"
+            size="large"
+            label="취소"
+            onClick={handleOpenBuy}
+          />
+          <Button
+            type={
+              userPoint && candle?.point && userPoint >= candle?.point
+                ? 'default'
+                : 'disabled'
+            }
+            size="large"
+            label="구매하기"
+            onClick={() => {
+              setOpenBuy(false);
+              handleBuyCandle(candle?.point ? candle.point : 0);
+            }}
+          />
+        </div>
+      </Modal>
+      <Modal open={openSuccess}>
+        <img
+          style={{ width: '5rem', height: '5rem' }}
+          src={candle?.imageUrl}
+          alt={candle?.candleId.toString()}
+        />
+        <h3>장식초 구매가 완료되었습니다.</h3>
+        <span>남은 포인트: {userPoint}P</span>
+        <Button
+          type="default"
+          label="확인"
+          onClick={() => {
+            navigate(`/letter/create/${ownerId}?candleId=${candle?.candleId}`);
+          }}
+        />
+      </Modal>
+      <LoginModal open={openLogin} handleOpen={handleOpenLogin} />
     </Wrapper>
   );
 };
