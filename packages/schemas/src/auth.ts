a33f00a27fda9920a28ed17c3a2209c8af367e93
ieun32 <ueunviss@gmail.com>
2024-07-16 10:18:53 +0900

    Refactor: zod UserType date 타입 추가

--- packages/schemas/src/auth.ts
+++ packages/schemas/src/auth.ts
@@ -22,13 +22,13 @@ export const UserType = z.object({
   id: z.string(),
   password: z.union([z.string(), z.null()]),
   nickname: z.string(),
-  birthday: z.string(),
+  birthday: z.union([z.string(), z.date()]),
   point: z.number(),
   loginType: z.string(),
   sheetColor: z.union([z.string(), z.null()]),
   creamColor: z.union([z.string(), z.null()]),
-  createdAt: z.string(),
-  updatedAt: z.string(),
+  createdAt: z.union([z.string(), z.date()]),
+  updatedAt: z.union([z.string(), z.date()]),
 });
 
 export type UserType = z.infer<typeof UserType>;
