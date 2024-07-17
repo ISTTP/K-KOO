e5412b0ea6f5b73c03f1e65bcd21609ca0d0e55a
kyr4601 <jane4545@naver.com>
2024-07-17 21:45:13 +0900

    Feat: 회원 가입 연도 정보 제공 api

--- apps/server/src/routes/user.ts
+++ apps/server/src/routes/user.ts
@@ -11,4 +11,12 @@ router.get('/user/me', authorize, async (req, res) => {
   res.status(200).json(data);
 });
 
+router.get('/user/year', authorize, async (req, res) => {
+  const userId = req.userId;
+  const data = await getUser(userId);
+
+  const startYear = data?.createdAt.getFullYear();
+  res.status(200).json(startYear);
+});
+
 export default router;
