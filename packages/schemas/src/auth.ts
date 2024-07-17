0b721142d6bab167b2e35e6f201f159a2f7e7d70
ieun32 <ueunviss@gmail.com>
2024-07-17 15:44:03 +0900

    Chore: user zod type 수정 및 파일 이동

--- packages/schemas/src/auth.ts
+++ packages/schemas/src/auth.ts
@@ -17,22 +17,6 @@ export const SocialLoginFailType = z.object({
 
 export type SocialLoginFailType = z.infer<typeof SocialLoginFailType>;
 
-export const UserType = z.object({
-  userId: z.string(),
-  id: z.string(),
-  password: z.union([z.string(), z.null()]),
-  nickname: z.string(),
-  birthday: z.union([z.string(), z.date()]),
-  point: z.number(),
-  loginType: z.string(),
-  sheetColor: z.union([z.string(), z.null()]),
-  creamColor: z.union([z.string(), z.null()]),
-  createdAt: z.union([z.string(), z.date()]),
-  updatedAt: z.union([z.string(), z.date()]),
-});
-
-export type UserType = z.infer<typeof UserType>;
-
 export const BirthdayType = z.object({
   birthday: z.date(),
 });
