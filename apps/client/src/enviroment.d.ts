dba5a07a2357e8ae30340994ef0a108276fb3dee
kyr4601 <jane4545@naver.com>
2024-07-18 20:39:28 +0900

    Feat: FCM 관련 env 추가

--- apps/client/src/enviroment.d.ts
+++ apps/client/src/enviroment.d.ts
@@ -3,6 +3,14 @@ export declare global {
   namespace NodeJS {
     interface ProcessEnv {
       SERVER_URL: string;
+      FIREBASE_VAPID_KEY: string;
+      API_KEY: string;
+      AUTH_DOMAIN: string;
+      PROJECT_ID: string;
+      STORAGE_BUCKET: string;
+      MESSAGING_SENDER_ID: string;
+      APP_ID: string;
+      MEASUREMENT_ID: string;
     }
   }
 }
