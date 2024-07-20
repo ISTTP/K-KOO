670ebcd53253efcc32959fc8162a13e7be69cdcb
ieun32 <ueunviss@gmail.com>
2024-07-20 18:05:30 +0900

    Chore: 웹팩 번들 어날라이저 플러그인 설치 및 적용

--- apps/client/webpack.config.prod.js
+++ apps/client/webpack.config.prod.js
@@ -2,7 +2,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
