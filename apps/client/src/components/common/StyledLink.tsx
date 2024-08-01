fbaf91e20a89f1a6b9c631a58bfd0691da32e74f
ieun32 <ueunviss@gmail.com>
2024-08-01 09:42:46 +0900

    Feat: 링크 컴포넌트 생성

---
+++ apps/client/src/components/common/StyledLink.tsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+  font-size: 0.8rem;
+  color: var(--gray-500);
+`;
+
+export default StyledLink;
