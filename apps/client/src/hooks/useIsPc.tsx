01ced7702c22a2f144660b9aed5744d9f296339b
kyr4601 <jane4545@naver.com>
2024-08-03 18:34:36 +0900

    Chore: useisPc hook 기준 수정

--- apps/client/src/hooks/useIsPc.tsx
+++ apps/client/src/hooks/useIsPc.tsx
@@ -3,25 +3,26 @@ import { debounce } from 'lodash';
 
 /*
  * useIsPC
- * width 값을 가져와서 width가 800이상이면 true, 아니면 false를 돌려준다.
+ * width 값을 가져와서 width가 point 이상이면 true, 아니면 false를 돌려준다.
  */
-function useIsPC(): boolean {
+function useIsPC(point: number): boolean {
   const [width, setWidth] = useState<number>(window.innerWidth);
-  const [isPC, setIsPC] = useState<boolean>(true);
+  const [isPC, setIsPC] = useState<boolean>(window.innerWidth > point);
 
   useEffect(() => {
     const handleResize = debounce(() => {
       setWidth(window.innerWidth);
     }, 0);
-    window.addEventListener(`resize`, handleResize);
+
+    window.addEventListener('resize', handleResize);
     return () => {
-      window.removeEventListener(`resize`, handleResize);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   useEffect(() => {
-    setIsPC(width > 800);
-  }, [width]);
+    setIsPC(width > point);
+  }, [width, point]);
 
   return isPC;
 }
