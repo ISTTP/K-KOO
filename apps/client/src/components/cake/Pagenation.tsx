1ff3eeae1aed6ff05bf504adc7fd3f8ee8059860
kyr4601 <jane4545@naver.com>
2024-08-03 10:48:57 +0900

    Chore: 페이지네이션 스타일 변경

--- apps/client/src/components/cake/Pagenation.tsx
+++ apps/client/src/components/cake/Pagenation.tsx
@@ -14,24 +14,22 @@ const Pagenation: React.FC<PageProps> = ({
 }) => {
   return (
     <PageContainer>
-      <div>
-        <button
-          onClick={() => changePage(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          &lt;
-        </button>
-        <span>
-          {' '}
-          {currentPage} / {totalPage}{' '}
-        </span>
-        <button
-          onClick={() => changePage(currentPage + 1)}
-          disabled={currentPage === totalPage}
-        >
-          &gt;
-        </button>
-      </div>
+      <button
+        onClick={() => changePage(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        &lt;
+      </button>
+      <span>
+        {' '}
+        {currentPage} / {totalPage}{' '}
+      </span>
+      <button
+        onClick={() => changePage(currentPage + 1)}
+        disabled={currentPage === totalPage}
+      >
+        &gt;
+      </button>
     </PageContainer>
   );
 };
@@ -41,4 +39,29 @@ export default Pagenation;
 const PageContainer = styled.div`
   margin-top: 20px;
   text-align: center;
+  gap: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 62px;
+
+  button{
+    width: 29px;
+    height: 29px;
+    border-radius: 50%;
+    border: none;
+    background: var(--orange-500);
+    font-family: Pretendard;
+    color: var(--white);
+  }
+
+  span{
+    color: #000;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
 `;
