0cf82235647273e843645393f3039c46b77640e7
ieun32 <ueunviss@gmail.com>
2024-08-04 10:32:39 +0900

    Feat: 새로운 유틸 함수 내보내기

--- apps/client/src/utils/index.ts
+++ apps/client/src/utils/index.ts
@@ -1,5 +1,15 @@
 import { checkBirthdayWithin30Days } from "./checkBirthdayWithin30Days";
 import { hashPassword } from "./hashPassword";
+import { hideUserId } from "./hideUserId";
+import { formatDate } from "./formatDate";
+import { hideEmail } from "./hideEmail";
 import { handleButtonClick } from "./handleButtonClick";
 
-export { checkBirthdayWithin30Days, hashPassword, handleButtonClick };
+export {
+  checkBirthdayWithin30Days,
+  hashPassword,
+  handleButtonClick,
+  hideUserId,
+  formatDate,
+  hideEmail
+};
