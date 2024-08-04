bc71b13e2b3432750747ca82a26bd02d1c584366
ieun32 <ueunviss@gmail.com>
2024-08-04 10:39:12 +0900

    Feat: 비밀번호 해싱 함수 작성

--- apps/server/src/service/verify.ts
+++ apps/server/src/service/verify.ts
@@ -1,7 +1,9 @@
-//만료된 인증 번호 삭제 (3분이 지난)
+import 'dotenv/config';
 import prisma from '@isttp/db/all';
 import cron from 'node-cron';
+import { SHA256 } from 'crypto-js';
 
+//만료된 인증 번호 삭제 (3분이 지난)
 export async function deleteExpiredVerifyCode() {
   console.log(':: 만료된 인증 코드 삭제 스케줄러 시작 ::');
   cron.schedule('*/1 * * * *', async () => {
@@ -14,4 +16,11 @@ export async function deleteExpiredVerifyCode() {
       },
     });
   });
+}
+
+export function hashPassword(password: string) {
+  const salt = process.env.PASSWORD_SALT;
+  const combined = password + salt;
+  const hashedPassword = SHA256(combined).toString();
+  return hashedPassword;
 }
