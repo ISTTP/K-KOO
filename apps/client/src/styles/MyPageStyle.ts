d3e63ca6b2262f51f1f1a190a36c8a18249ab54f
ieun32 <ueunviss@gmail.com>
2024-08-08 10:44:56 +0900

    Design: 마이페이지 디자인

--- apps/client/src/styles/MyPageStyle.ts
+++ apps/client/src/styles/MyPageStyle.ts
@@ -6,6 +6,7 @@ export const MyPageHeader = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  margin-bottom: 4rem;
 `;
 
 export const HomeButton = styled.button`
@@ -18,12 +19,13 @@ export const HomeButton = styled.button`
   }
 `;
 
-export const NameWrapper = styled.h2`
+export const TitleWrapper = styled.h2`
   display: flex;
-  flex-direction: row;
-  justify-content: start;
-  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
   width: 100%;
+  gap: 0.5rem;
 `;
 
 export const Nickname = styled.span`
@@ -36,7 +38,31 @@ export const InfoWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 100%;
-  margin: 0.5rem 0;
+`;
+
+export const LabelWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+export const EditContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 2rem;
+`;
+
+export const EditWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const MyLetterButton = styled.button`
@@ -85,19 +111,24 @@ export const SubTitle = styled.h2`
 
 export const Hr = styled.hr`
   width: 100%;
-  border: 1.5px solid var(--gray-300);
-  margin: 2rem 0;
+  border: 1px solid var(--orange-400);
+  margin: 1.6rem 0;
 `;
 
 export const Input = styled.input`
   width: 100%;
+  height: 3.5rem;
   padding: 0.5rem;
   margin: 0.5rem 0;
-  border: 1px solid var(--gray-400);
-  border-radius: 0.5rem;
+  padding: 0 1rem;
+  font-size: 1.05rem;
+  border-radius: 0.25rem;
+  border: 1px solid var(--orange-400);
+  background-color: var(--white);
+  color: var(--black);
 
   &:disabled {
-    background-color: var(--gray-300);
+    color: var(--gray-500);
   }
 `;
 
@@ -107,7 +138,6 @@ export const AccountSettingWrapper = styled.div`
   justify-content: start;
   align-items: start;
   width: 100%;
-  gap: 1rem;
 `;
 
 export const AccountButton = styled.button`
@@ -116,6 +146,7 @@ export const AccountButton = styled.button`
   border: none;
   background-color: transparent;
   cursor: pointer;
+  margin-bottom: 1.6rem;
 
   &:hover {
     opacity: 0.8;
@@ -138,6 +169,42 @@ export const StyledLink = styled(Link)`
   color: var(--black);
   cursor: pointer;
   text-decoration: none;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: right;
+  gap: 1rem;
+`;
+
+export const OrangeButton = styled.button`
+  background-color: var(--orange-500);
+  border: none;
+  border-radius: 0.8rem;
+  padding: 0.3rem 1rem;
+  cursor: pointer;
+  font-size: 1rem;
+  color: var(--white);
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const WhiteButton = styled.button`
+  background-color: var(--white);
+  border: 1px solid var(--orange-500);
+  border-radius: 0.8rem;
+  padding: 0.3rem 1rem;
+  cursor: pointer;
+  font-size: 1rem;
+  color: var(--orange-500);
 
   &:hover {
     opacity: 0.8;
