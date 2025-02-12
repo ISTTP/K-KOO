37215b660b74e7748f9f9f7e76a3ebed70fe3992
kyr4601 <jane4545@naver.com>
2025-02-12 18:34:16 +0900

    fix: axios interceptor 401 에러처리 로직 수정

--- apps/client/src/apis/axios.ts
+++ apps/client/src/apis/axios.ts
@@ -14,8 +14,12 @@ axiosInstance.interceptors.response.use(
     const axiosError = customError.response?.status as number;
 
 
-    // 401 에러 응답
-    if (axiosError === 401) {
+    // 401 에러 응답 (인가 미들웨어에서 모든 토큰이 만료되었을때 401 리턴 -> 로그인 유도)
+    if ((axiosError === 401) && (window.location.pathname !== '/')) {
+      if (/^\/cake\/[^/]+$/.test(window.location.pathname)) {
+        return Promise.reject(error);
+      }
+      window.location.replace('/');
       return Promise.reject(error);
     }
 
@@ -29,7 +33,6 @@ axiosInstance.interceptors.response.use(
     if (axiosError === 404 || 400) {
       return Promise.reject(error);
     }
-
 
     return Promise.reject(error);
   },
